ea001a31122accbedd2bbc613eafddda33b9c4f4
Arjun Azavedo <arjun.azavedo22@gmail.com>
2020-05-05 06:55:39 -0500

    added topic html

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -7,5 +7,7 @@ from . import views
 app_name = 'learning_logs'
 urlpatterns = [
     # Home page
-    path('', views.index, name='index'), 
+    path('', views.index, name='index'),
+    # Page that shows all topics.
+    path('topics/', views.topics, name='topics'),
 ]
