da40dd2719f7ed30bbe8763018d8212c608feec1
Arjun Azavedo <arjun.azavedo22@gmail.com>
2020-05-05 06:42:45 -0500

    added shell

---
+++ MyShell.py
@@ -0,0 +1,22 @@
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "learning_log.settings")
+
+import django
+django.setup()
+
+from learning_logs.models import Topic
+
+topics = Topic.objects.all()
+
+for topic in topics:
+    print(topic.id, topic)
+
+t = Topic.objects.get(id=1)
+print(t.text)
+print(t.date_added)
+
+entries = t.entry_set.all()
+
+for entry in entries:
+    print(entry)
+    
