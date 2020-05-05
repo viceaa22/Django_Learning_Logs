fc9c0ba95b5738c2d705ddad74dc3df03d0945fb
Arjun Azavedo <arjun.azavedo22@gmail.com>
2020-05-05 06:50:03 -0500

    added view

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+def index(request):
+    """The home page for Learning Log."""
+    return render(request, 'learning_logs/index.html')
+
+
 # Create your views here.
