e2c37aba920641193c211aac2a9f19f6f20b76a5
Arjun Azavedo <arjun.azavedo22@gmail.com>
2020-05-05 06:57:14 -0500

    edited views

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -6,10 +6,10 @@ def index(request):
     """The home page for Learning Log."""
     return render(request, 'learning_logs/index.html')
 
- def topics(request):
-     """Show all topics."""
-     topics = Topic.objects.order_by('date_added')
-     context = {'topics': topics}
-     return render(request, 'learning_logs/topics.html', context)
+def topics(request):
+    """Show all topics."""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
 
 # Create your views here.
